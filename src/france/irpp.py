f66e96aebd04e49400b1c72187727bf4efb08f54
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2012-01-30 21:00:47 +0100

    bug in ppe (_ppe)

--- src/france/irpp.py
+++ src/france/irpp.py
@@ -987,7 +987,7 @@ def _ppe_elig_i(ppe_rev, ppe_coef_tp, _P):
     eligibilité individuelle à la ppe
     '''
     P = _P.ir.credits_impot.ppe
-    return (ppe_rev >= P.seuil1)&(_ppe_coef_tp!=0)
+    return (ppe_rev >= P.seuil1)&(ppe_coef_tp!=0)
 
 def _ppe(ppe_elig, ppe_elig_i, ppe_rev, ppe_base, ppe_coef, ppe_coef_tp, nb_pac, marpac, celdiv, veuf, caseT, caseL, nbH, _P, _option = {'ppe_elig_i': ALL, 'ppe_base': ALL, 'ppe_rev': ALL, 'ppe_coef_tp': ALL}):
     '''
