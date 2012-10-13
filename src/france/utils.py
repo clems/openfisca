9d1d33e350b5aeef59fd4f5197b3ab3228bd9205
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2012-10-14 00:38:43 +0200

    closes #75 by changing so in Scenario Various fixes to tunisia stuff

--- src/france/utils.py
+++ src/france/utils.py
@@ -42,7 +42,7 @@ class Scenario(object):
         self.famille = {}
         
         # menage est un dict de dict la clé est la pref
-        self.menage = {0:{'loyer':500,'so':3, 'code_postal':69001, 'zone_apl':2}}
+        self.menage = {0:{'loyer':500,'so':4, 'code_postal':69001, 'zone_apl':2}}
 
         # on ajoute un individu, déclarant et chef de famille
         self.addIndiv(0, datetime(1975,1,1).date(), 'vous', 'chef')
